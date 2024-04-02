74d0d885229d7c157eb763c55800c8597aa2e6af
Ayush Patel <109383229+Phoenix-Ayush@users.noreply.github.com>
2024-04-02 20:29:19 +0530

    Delete Note_Taking Backend directory

--- Note_Taking Backend/modules/note.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const noteSchema = mongoose.Schema({
-    title: {type: String, require: true},
-    content: {type: String, require: true}
-});
-
-module.exports = mongoose.model('Note', noteSchema);
